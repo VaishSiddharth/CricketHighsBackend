e4471fb7d6d702dddb161843e5be62586d7ff658
Siddharth Vaish <sidvaish97@gmail.com>
2019-08-08 22:45:25 +0530

    frame by frame video make from images

--- cut_video/makevideo.py
+++ cut_video/makevideo.py
@@ -4,6 +4,40 @@ import mss
 import numpy
 import os
 
+from os.path import isfile, join
+
+
+def convert_frames_to_video(pathIn, pathOut, fps):
+    frame_array = []
+    files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
+
+    # for sorting the file names properly
+    files.sort(key=lambda x: int(x[5:-4]))
+
+    for i in range(len(files)):
+        filename = pathIn + files[i]
+        # reading each files
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width, height)
+        print(filename)
+        # inserting the frames into an image array
+        frame_array.append(img)
+
+    out = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+
+    for i in range(len(frame_array)):
+        # writing to a image array
+        out.write(frame_array[i])
+    out.release()
+
+
+def mainvideos():
+    pathIn = 'images/'
+    pathOut = 'video.avi'
+    fps = 25.0
+    convert_frames_to_video(pathIn, pathOut, fps)
+
 
 
 def save_images(*args, **kwargs):
@@ -42,7 +76,7 @@ def save_images(*args, **kwargs):
                     cv2.destroyAllWindows()
                     break
             print("fps: {}".format(1 / (time.time() - last_time)))
-        make_video()
+        mainvideos()
 
 
 def make_video(*args, **kwargs):
